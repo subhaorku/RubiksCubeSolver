361e3cd88981ffd0bda4e9958fe588134258b5f2
subhaorku <guptasubhasini68@gmail.com>
2024-06-13 02:04:40 +0530

    fixing_base class

--- RubiksCube.cpp
+++ RubiksCube.cpp
@@ -1,5 +1,356 @@
-//
-// Created by ASUS on 6/7/2024.
-//
-
 #include "RubiksCube.h"
+
+/*
+ * Definition of member function 'getColorLetter' for the 'RubiksCube' class
+ * This function takes a COLOR enumeration value as input and
+ * returns the corresponding first letter of the color as a character.
+ */
+char RubiksCube::getColorLetter(COLOR color) {
+    switch(color){
+        case COLOR::BLUE:
+            return 'B';
+        case COLOR::GREEN:
+            return 'G';
+        case COLOR::RED:
+            return 'R';
+        case COLOR::YELLOW:
+            return 'Y';
+        case COLOR::WHITE:
+             return 'W';
+        case COLOR::ORANGE:
+            return 'O';
+    }
+}
+// :: helps avoid ambiguity by clearly indicating that RED is a member of COLOR, which is an enumeration inside the RubiksCube class.
+
+
+/*
+ * Describe a Move using an index
+ * This function, getMove, is a member function of the RubiksCube class.
+ * It takes a MOVE enumeration value as input and returns the corresponding string representation of that move.
+ */
+
+string RubiksCube::getMove(RubiksCube::MOVE ind) {
+    switch(ind){
+        case MOVE::L:
+            return "L";
+        case MOVE::LPRIME:
+            return "L'";
+        case MOVE::L2:
+            return "L2";
+        case MOVE::R:
+            return "R";
+        case MOVE::RPRIME:
+            return "R'";
+        case MOVE::R2:
+            return "R2";
+        case MOVE::U:
+            return "U";
+        case MOVE::UPRIME:
+            return "U'";
+        case MOVE::U2:
+            return "U2";
+        case MOVE::D:
+            return "D";
+        case MOVE::DPRIME:
+            return "D'";
+        case MOVE::D2:
+            return "D2";
+        case MOVE::F:
+            return "F";
+        case MOVE::FPRIME:
+            return "F'";
+        case MOVE::F2:
+            return "F2";
+        case MOVE::B:
+            return "B";
+        case MOVE::BPRIME:
+            return "B'";
+        case MOVE::B2:
+            return "B2";
+
+    }
+}
+
+
+
+/*
+ * Perform a move operation on using a Move index.
+ * This function, move, is a member function of the RubiksCube class.
+ * It performs a move operation on the Rubik's Cube based on the specified move index (MOVE enumeration value).
+ * Return Type: RubiksCube& - The function returns a reference to the RubiksCube object, allowing for method chaining.
+ * For each case, the function calls the appropriate move operation (l(), lPrime(), l2(), etc.) on the current RubiksCube object (this) and returns a reference to it.
+ */
+RubiksCube &RubiksCube::move(MOVE ind){
+    switch(ind) {
+        case MOVE::L:
+            return this->l();
+        case MOVE::LPRIME:
+            return this->lPrime();
+        case MOVE::L2:
+            return this->l2();
+        case MOVE::R:
+            return this->r();
+        case MOVE::RPRIME:
+            return this->rPrime();
+        case MOVE::R2:
+            return this->r2();
+        case MOVE::U:
+            return this->u();
+        case MOVE::UPRIME:
+            return this->uPrime();
+        case MOVE::U2:
+            return this->u2();
+        case MOVE::D:
+            return this->d();
+        case MOVE::DPRIME:
+            return this->dPrime();
+        case MOVE::D2:
+            return this->d2();
+        case MOVE::F:
+            return this->f();
+        case MOVE::FPRIME:
+            return this->fPrime();
+        case MOVE::F2:
+            return this->f2();
+        case MOVE::B:
+            return this->b();
+        case MOVE::BPRIME:
+            return this->bPrime();
+        case MOVE::B2:
+            return this->b2();
+
+    }
+}
+
+
+/*
+ * Invert a Move
+ */
+RubiksCube &RubiksCube::invert(MOVE ind) {
+    switch (ind) {
+        case MOVE::L:
+            return this->lPrime();
+        case MOVE::LPRIME:
+            return this->l();
+        case MOVE::L2:
+            return this->l2();
+        case MOVE::R:
+            return this->rPrime();
+        case MOVE::RPRIME:
+            return this->r();
+        case MOVE::R2:
+            return this->r2();
+        case MOVE::U:
+            return this->uPrime();
+        case MOVE::UPRIME:
+            return this->u();
+        case MOVE::U2:
+            return this->u2();
+        case MOVE::D:
+            return this->dPrime();
+        case MOVE::DPRIME:
+            return this->d();
+        case MOVE::D2:
+            return this->d2();
+        case MOVE::F:
+            return this->fPrime();
+        case MOVE::FPRIME:
+            return this->f();
+        case MOVE::F2:
+            return this->f2();
+        case MOVE::B:
+            return this->bPrime();
+        case MOVE::BPRIME:
+            return this->b();
+        case MOVE::B2:
+            return this->b2();
+    }
+}
+
+/*
+ * Print function to print the current state of the Rubiks Cube in planar format
+ */
+void RubiksCube::print() const {
+    cout << "Rubik's Cube:\n\n";
+
+    for (int row = 0; row <= 2; row++) {
+        for (unsigned i = 0; i < 7; i++) cout << " ";
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::UP, row, col)) << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+
+    for (int row = 0; row <= 2; row++) {
+
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::LEFT, row, col)) << " ";
+        }
+        cout << " ";
+
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::FRONT, row, col)) << " ";
+        }
+        cout << " ";
+
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::RIGHT, row, col)) << " ";
+        }
+        cout << " ";
+
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::BACK, row, col)) << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+
+    for (int row = 0; row <= 2; row++) {
+        for (unsigned i = 0; i < 7; i++) cout << " ";
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::DOWN, row, col)) << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+/*
+ * randomShuffleCube is a member function of the RubiksCube Class
+ * It generates and returns the vector of random sequence of moves to shuffle the Rubik's Cube
+ * <RubiksCube::MOVE> => MOVE is an enum type defined within the RubiksCube class
+ */
+vector<RubiksCube::MOVE> RubiksCube::randomShuffleCube(unsigned int times) {
+    vector<MOVE> moves_performed;
+    srand(time(0));
+    // srand(time(0)) is a common way to seed the random number generator using the current time as a seed value.
+    for(unsigned int i=0;i<times;i++)
+    {
+        unsigned int selectMove = (rand()%18);
+        moves_performed.push_back(static_cast<MOVE>(selectMove));
+        this->move(static_cast<MOVE>(selectMove));
+    }
+    return moves_performed;
+}
+
+
+//Helper function which returns the string of the corner
+
+string RubiksCube::getCornerColorString(uint8_t ind) const {
+    string str="";
+    switch (ind) {
+//        UFR
+        case 0:
+            str += getColorLetter(getColor(FACE::UP, 2, 2));
+            str += getColorLetter(getColor(FACE::FRONT, 0, 2));
+            str += getColorLetter(getColor(FACE::RIGHT, 0, 0));
+            break;
+
+//            UFL
+        case 1:
+            str += getColorLetter(getColor(FACE::UP, 2, 0));
+            str += getColorLetter(getColor(FACE::FRONT, 0, 0));
+            str += getColorLetter(getColor(FACE::LEFT, 0, 2));
+            break;
+
+//            UBR
+        case 2:
+            str += getColorLetter(getColor(FACE::UP, 0, 0));
+            str += getColorLetter(getColor(FACE::BACK, 0, 2));
+            str += getColorLetter(getColor(FACE::LEFT, 0, 0));
+            break;
+
+//            UBL
+        case 3:
+            str += getColorLetter(getColor(FACE::UP, 0, 2));
+            str += getColorLetter(getColor(FACE::BACK, 0, 0));
+            str += getColorLetter(getColor(FACE::RIGHT, 0, 2));
+            break;
+
+//            DFR
+        case 4:
+            str += getColorLetter(getColor(FACE::DOWN, 0, 2));
+            str += getColorLetter(getColor(FACE::FRONT, 2, 2));
+            str += getColorLetter(getColor(FACE::RIGHT, 2, 0));
+            break;
+
+//            DFL
+        case 5:
+            str += getColorLetter(getColor(FACE::DOWN, 0, 0));
+            str += getColorLetter(getColor(FACE::FRONT, 2, 0));
+            str += getColorLetter(getColor(FACE::LEFT, 2, 2));
+            break;
+
+//            DBR
+        case 6:
+            str += getColorLetter(getColor(FACE::DOWN, 2, 2));
+            str += getColorLetter(getColor(FACE::BACK, 2, 0));
+            str += getColorLetter(getColor(FACE::RIGHT, 2, 2));
+            break;
+
+//            DBL
+        case 7:
+            str += getColorLetter(getColor(FACE::DOWN, 2, 0));
+            str += getColorLetter(getColor(FACE::BACK, 2, 2));
+            str += getColorLetter(getColor(FACE::LEFT, 2, 0));
+            break;
+    }
+    return str;
+}
+
+/*
+ * For given value of ind get the corner color as string
+ * iterate over char of that string to get the corner index
+ */
+uint8_t RubiksCube::getCornerIndex(uint8_t ind) const {
+    string corner = getCornerColorString(ind);
+
+    uint8_t ret = 0;
+    for (auto c: corner) {
+        if (c != 'W' && c != 'Y') continue;
+        if (c == 'Y') {
+            ret |= (1 << 2);
+        }
+    }
+
+    for (auto c: corner) {
+        if (c != 'R' && c != 'O') continue;
+        if (c == 'O') {
+            ret |= (1 << 1);
+        }
+    }
+
+    for (auto c: corner) {
+        if (c != 'B' && c != 'G') continue;
+        if (c == 'G') {
+            ret |= (1 << 0);
+        }
+    }
+    return ret;
+}
+
+
+/*
+ * get Corner Orientation
+ */
+
+uint8_t RubiksCube::getCornerOrientation(uint8_t ind) const {
+    string corner = getCornerColorString(ind);
+
+    string actual_str = "";
+
+    for (auto c: corner) {
+        if (c != 'W' && c != 'Y') continue;
+        actual_str.push_back(c);
+    }
+
+    if (corner[1] == actual_str[0]) {
+        return 1;
+    } else if (corner[2] == actual_str[0]) {
+        return 2;
+    } else return 0;
+}
